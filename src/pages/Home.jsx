602b1ea561f51d15728bc8e2125833dfe20191e9
Anthony <kojiro5656i@gmail.com>
2025-06-13 09:38:41 +0200

    Initialisation du Projet

---
+++ src/pages/Home.jsx
@@ -0,0 +1,4 @@
+function Home() {
+  return <h1>Bienvenue sur le site de l'architecte ABCDA</h1>;
+}
+export default Home;
---
+++ src/pages/ProjectDetail.jsx
@@ -0,0 +1,4 @@
+function ProjectDetail() {
+  return <h1>Détail du projet</h1>;
+}
+export default ProjectDetail;
