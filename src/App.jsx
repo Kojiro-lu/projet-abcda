f3f924eac0c32731f30b2acdcb888eb601b22e46
Anthony <kojiro5656i@gmail.com>
2025-06-13 10:02:59 +0200

    Réorganisation des pages et création du composant Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import ProjectDetail from "./pages/ProjectDetail.jsx";
+import Header from "./components/Header/Header.jsx";
+import Home from "./pages/Home/Home.jsx";
+import ProjectDetail from "./pages/ProjectDetail/ProjectDetail.jsx";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects/:id" element={<ProjectDetail />} />
