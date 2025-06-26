cb2ab83eb41c769a0bbc27d27abb0d60911887af
Anthony <kojiro5656i@gmail.com>
2025-06-26 10:54:07 +0200

    fix: modification de la maps google

--- src/components/Contact/ContactMap/ContactMap.jsx
+++ src/components/Contact/ContactMap/ContactMap.jsx
@@ -4,10 +4,10 @@ const ContactMap = () => {
   return (
     <div className="contact-map">
       <iframe
-        src="https://www.google.com/maps/embed?pb=..."
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5350.966436749672!2d-2.8351620240972624!3d47.888331868481!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48102f65be840463%3A0xf5627e18e0be791b!2sAtelier%20Bettremieux%20Cyril%20d&#39;Architecture%20SARL!5e0!3m2!1sfr!2sfr!4v1750763116312!5m2!1sfr!2sfr"
         allowFullScreen=""
         loading="lazy"
-        title="Localisation ABCDA"
+        title="Localisation ABCDA - Cabinet D'architecte - Cyril Bettremieux"
       ></iframe>
     </div>
   );
